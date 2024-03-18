c43ec500a44efb4f5c562629f473e0a22541276a
Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>
2024-03-18 13:59:00 -0600

    init repo
    Signed-off-by: Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>

---
+++ main.go
@@ -0,0 +1,11 @@
+// package main is the entry point
+package main
+
+import (
+	"github.com/datumforge/geodetic/cmd"
+	// _ "github.com/datumforge/geodetic/internal/ent/generated/runtime"
+)
+
+func main() {
+	cmd.Execute()
+}
