a06058a4340d904393d2817144eedde83f59aabd
Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>
2024-04-14 17:47:23 -0600

    feat:add goose migrations
    Signed-off-by: Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>

--- db/migrate.go
+++ db/migrate.go
@@ -1,5 +1,3 @@
-//go:build ignore
-
 // Package db provides an embedded filesystem containing all the database migrations
 package db
 
@@ -11,3 +9,8 @@ import (
 //
 //go:embed migrations/*.sql
 var Migrations embed.FS
+
+// GooseMigrations contain an embedded filesystem with all the goose migration files
+//
+//go:embed migrations-goose/*.sql
+var GooseMigrations embed.FS
